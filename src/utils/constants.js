9d7c3dd770ee036a29135076522a09fc4f27b96f
Harshkumarr911 <harshkumarr9251@gmail.com>
2025-11-03 20:53:27 +0530

    Updated UI, fixed API call issue

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,6 @@
-export const API_BASE_URL ='http://localhost:8080/api';
+
+
+export const API_BASE_URL =import.meta.env.VITE_API_URL || 'http://localhost:8080/api';
 
 export const ROLES = {
   ATHLETE: 'ATHLETE',
